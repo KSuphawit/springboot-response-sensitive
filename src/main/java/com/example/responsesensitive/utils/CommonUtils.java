d891bc496fb87373dbefdc97fc3642eb67116794
Ksuphawit <ksuphawit@Ksuphawits-MacBook-Pro.local>
2025-06-10 16:10:29 +0700

    test

--- src/main/java/com/example/responsesensitive/utils/CommonUtils.java
+++ src/main/java/com/example/responsesensitive/utils/CommonUtils.java
@@ -7,4 +7,8 @@ public class CommonUtils {
     public static boolean isPrimitiveOrString(Class<?> clazz) {
         return ClassUtils.isPrimitiveOrWrapper(clazz) || String.class == clazz;
     }
+
+    public static int add(int n1, int n2) {
+        return 1;
+    }
 }
